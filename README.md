d6c06288299f48104991bd9cfe0da84e446e7376
Dongjin Lee <djlee1509@gmail.com>
2020-01-12 23:21:56 +0000

    deleted unncessary codes from the previous ones.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Coding Challenge
 
+I went for Python - Flask for this challenge due to a Flask is a light and micro web framework where as Python - Django is more heavy (used where it requires admin management).
+
--- app.py
+++ app.py
@@ -1,41 +1,3 @@
-# from flask import Flask, request, render_template
-# import requests
-# import re
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# @app.route('/', methods=['POST'])
-# def response():
-#     headers = {
-#         "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
-#     }
-#     URL = request.form.get("url")
-
-#     try:
-#         page = requests.get(URL, headers=headers)
-#     except:
-#         print("Error while opening the URL")
-
-#     words = page.text.replace('\n', ' ').split()
-#     words = [word.lower() for word in words]
-
-#     dictionary = dict()
-#     regex = r"([^a-z'+])"
-
-#     for word in words[:100]:
-#         word = re.sub(regex, "", word)
-#         counter = words.count(word)
-#         dictionary[word] = counter
-
-#     return render_template('index.html', dictionary=dictionary)
-
-# if __name__ == '__main__':
-#     app.run()
-
 from flask import Flask, request, render_template
 import requests
 import re
@@ -56,13 +18,12 @@ def response():
     page = requests.get(url, headers=headers)
    
     words = page.text.replace('\n', ' ').split()
-
     words = [word.lower() for word in words]
 
     dictionary = dict()
     regex = r"([^a-z'+])"
 
-    for word in words:
+    for word in words[:100]:
         word = re.sub(regex, "", word)
         counter = words.count(word)
         dictionary[word] = counter
